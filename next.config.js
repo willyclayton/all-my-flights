ca65b9d52e3aeb1547534682648547fc39609d08
willyclayton <willyclayton04@gmail.com>
2026-03-04 18:29:10 -0600

    update configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: [
+    'recharts',
+    'react-smooth',
+    'react-simple-maps',
+    'd3-array',
+    'd3-color',
+    'd3-format',
+    'd3-geo',
+    'd3-interpolate',
+    'd3-path',
+    'd3-scale',
+    'd3-shape',
+    'd3-time',
+    'd3-time-format',
+    'internmap',
+    'topojson-client',
+  ],
+}
 
 module.exports = nextConfig
